eed3e6726754aaaa1da399e8d6c353cf51c2892e
MubashirACode <mubashirali200512@gmail.com>
2026-01-26 01:51:39 +0500

    add changes

--- backend/lib/prisma.ts
+++ backend/lib/prisma.ts
@@ -1,23 +1,11 @@
 import "dotenv/config";
 import { PrismaPg } from '@prisma/adapter-pg'
-  // ✅ Change this line
-import pkg from 'pg';
 import { PrismaClient } from "../generated/prisma/client.js";
-const { Pool } = pkg;
 
-const connectionString = process.env.DATABASE_URL!;
 
-const pool = new Pool({ connectionString });
-const adapter = new PrismaPg(pool);
+const connectionString = `${process.env.DATABASE_URL}`
 
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined
-}
+const adapter = new PrismaPg({ connectionString })
+const prisma = new PrismaClient({ adapter })
 
-const prisma = globalForPrisma.prisma ?? new PrismaClient({ adapter });
-
-if (process.env.NODE_ENV !== 'production') {
-  globalForPrisma.prisma = prisma;
-}
-
-export default prisma;  
+export default prisma;
